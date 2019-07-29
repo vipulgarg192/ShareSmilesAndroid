7f5510c7a52c7f9bf3e84aafab1a0e11deeb5159
Vipulgarg192 <vipulgarg192@gmail.com>
2019-07-28 22:10:09 -0400

    Created Users Modal

---
+++ app/src/main/java/com/cipher/sharesmilesandroid/modals/Users.java
@@ -0,0 +1,82 @@
+package com.cipher.sharesmilesandroid.modals;
+
+public class Users  {
+
+    private int userID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String dob;
+    private String gender;
+    private int userType;
+
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public int getUserType() {
+        return userType;
+    }
+
+    public void setUserType(int userType) {
+        this.userType = userType;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFullName(){
+        return firstName+" "+lastName;
+    }
+}
